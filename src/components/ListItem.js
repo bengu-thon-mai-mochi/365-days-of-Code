2891551f60c4689ea7090c4625da4f93819d6c83
bengu ozakinci <mochi@bengus-MacBook-Air.local>
2022-10-23 18:54:52 +0300

    Day 4

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,14 +1,11 @@
 function ListItem({ content }) {
-    console.log(content)
   return (
     <div className="card">
         <h1>Day: {content.id}</h1>
         <p> {content.day} -  {content.date}</p>
-
         {
             content.content.map(el => <li key="content.id"> { el }</li>)
         }
-        
         {content.note.length > 0 
             ? <p><b>Note:</b> {content.note}</p> 
             : <p>Nothing to note today</p>
